90c8a83ffdb8649a7c18fd274e23da4c22f143c7
OnionGL <wikmil44@mail.ru>
2022-05-15 20:58:03 +0300

    rebase

--- src/Component/ExchangeRates/ExchangeRates.tsx
+++ src/Component/ExchangeRates/ExchangeRates.tsx
@@ -43,7 +43,7 @@ export default function ExchangeRates() {
                <MenuItem value={"USD"}>USD</MenuItem>
                <MenuItem value={"EUR"}>EUR</MenuItem>
                <MenuItem value={"AED"}>AED</MenuItem>
-               <MenuItem value={"GBP"}>GsP</MenuItem>
+               <MenuItem value={"GBP"}>GBP</MenuItem>
         </Select>
         <Table sx={{ maxWidth: '80%' }} aria-label="caption table">
         <TableHead>
